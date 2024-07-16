ab50e02aab2dd4b0b2fef92cbf0fdb5cb7712b86
divyavinod6 <thisisdivyavinod@gmail.com>
2024-07-16 23:37:27 +0530

    updated root plugins

--- myapp/src/components/Header/index.js
+++ myapp/src/components/Header/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './style.css';
 function Header() {
-  return <div className="navbar">Header</div>;
+  return <div className="navbar">this is header</div>;
 }
 
 export default Header;
