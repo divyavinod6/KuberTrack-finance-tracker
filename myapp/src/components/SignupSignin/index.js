322c48364fc029d35227861f856e5ef132c24347
divyavinod6 <thisisdivyavinod@gmail.com>
2024-07-18 23:50:33 +0530

    created signup page

--- myapp/src/components/SignupSignin/index.js
+++ myapp/src/components/SignupSignin/index.js
@@ -2,11 +2,45 @@ import React, { useState } from 'react';
 import './style.css';
 import Input from '../Input';
 import Button from '../Button';
+import { auth } from '../../firebase';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { toast } from 'react-toastify';
 function SignupSigninComponent() {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
+
+  function signupWithEmail() {
+    console.log('Name', name);
+    console.log('email', email);
+    console.log('password', password);
+    console.log('confirmPassword', confirmPassword);
+    // Authenticate new user, or create a new account using email
+    if (
+      name !== '' &&
+      email !== '' &&
+      password !== '' &&
+      confirmPassword !== ''
+    ) {
+      createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+          // Signed up
+          const user = userCredential.user;
+          console.log('User>>>', user);
+          toast.success('User created!');
+          // ...
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          toast.error(errorMessage);
+          // ..
+        });
+    } else {
+      toast.error('All fields are mandatory');
+    }
+  }
   return (
     <div className="signup-wrapper">
       <h2 className="title">
@@ -20,12 +54,14 @@ function SignupSigninComponent() {
           placeholder={'John Doe'}
         />
         <Input
+          type="email"
           label={'Email'}
           state={email}
           setState={setEmail}
           placeholder={'JohnDoe@gmail.com'}
         />
         <Input
+          type="password"
           label={'Password'}
           state={password}
           setState={setPassword}
@@ -37,7 +73,10 @@ function SignupSigninComponent() {
           setState={setConfirmPassword}
           placeholder={'Example@123'}
         />
-        <Button text={'SignUp using Email and Password'} />
+        <Button
+          text={'SignUp using Email and Password'}
+          onClick={signupWithEmail}
+        />
         <p style={{ textAlign: 'center', margin: 0 }}>or</p>
         <Button text={'SignUp using Google'} blue={true} />
       </form>
