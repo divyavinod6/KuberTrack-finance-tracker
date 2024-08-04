874f190fb377288060c03cdd6587af5483fbe87e
divyavinod6 <thisisdivyavinod@gmail.com>
2024-08-04 23:27:24 +0530

    updated Dashboard

--- myapp/src/pages/Dashboard.js
+++ myapp/src/pages/Dashboard.js
@@ -1,27 +1,33 @@
 import React, { useState } from 'react';
 import Header from '../components/Header';
 import Cards from '../components/Cards';
-import { Modal } from 'antd';
 import AddIncomeModal from '../components/Modals/addIncome';
 import AddExpenseModal from '../components/Modals/addExpense';
+
 function Dashboard() {
   const [isExpenseModalVisible, setIsExpenseModalVisible] = useState(false);
   const [isIncomeModalVisible, setIsIncomeModalVisible] = useState(false);
+
   const showExpenseModal = () => {
     setIsExpenseModalVisible(true);
   };
+
   const showIncomeModal = () => {
     setIsIncomeModalVisible(true);
   };
+
   const handleExpenseCancel = () => {
     setIsExpenseModalVisible(false);
   };
+
   const handleIncomeCancel = () => {
     setIsIncomeModalVisible(false);
   };
+
   const onFinish = (values, type) => {
     console.log('On Finish', values, type);
   };
+
   return (
     <div>
       <Header />
@@ -35,28 +41,10 @@ function Dashboard() {
         onFinish={onFinish}
       />
       <AddIncomeModal
-        isExpenseModalVisible={isIncomeModalVisible}
-        handleExpenseCancel={handleIncomeCancel}
+        isIncomeModalVisible={isIncomeModalVisible}
+        handleIncomeCancel={handleIncomeCancel}
         onFinish={onFinish}
       />
-      {/* <Modal
-        style={{ fontWeight: 600 }}
-        title="Add Expenses"
-        onCancel={handleIncomeCancel}
-        footer={null}
-        visible={isIncomeModalVisible}
-      >
-        Income
-      </Modal>
-      <Modal
-        style={{ fontWeight: 600 }}
-        title="Add Expenses"
-        onCancel={handleExpenseCancel}
-        footer={null}
-        visible={isExpenseModalVisible}
-      >
-        Expense
-      </Modal> */}
     </div>
   );
 }
