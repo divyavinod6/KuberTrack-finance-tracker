b246fdaadec418e9641a6fc4022e7d1229c48c23
divyavinod6 <thisisdivyavinod@gmail.com>
2024-09-03 23:55:18 +0530

    updated index.js

--- myapp/src/components/TransactionTable/index.js
+++ myapp/src/components/TransactionTable/index.js
@@ -51,42 +51,54 @@ function TransactionTable({ transactions }) {
     }
   });
   return (
-    <>
-      <div className="input-flex">
-        <img src={searchImg} width="16" />
-        <input
-          value={search}
-          onChange={(e) => setSearch(e.target.value)}
-          placeholder="Search by name"
-        />
+    <div style={{ width: '100%', padding: '0rem 2rem' }}>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          gap: '1rem',
+          alignItems: 'center',
+          marginBottom: '1rem',
+        }}
+      >
+        <div className="input-flex">
+          <img src={searchImg} width="16" />
+          <input
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            placeholder="Search by name"
+          />
+          <input
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            placeholder="Search by name"
+          />
+        </div>
+        <Select
+          className="select-input"
+          onChange={(value) => setTypeFilter(value)}
+          value={typeFilter}
+          placeholder="Filter"
+          allowClear
+        >
+          <Option value="">All</Option>
+          <Option value="income">Income </Option>
+          <Option value="expense">Expense </Option>
+        </Select>
       </div>
-      <input
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
-        placeholder="Search by name"
-      />
-      <Select
-        className="select-input"
-        onChange={(value) => setTypeFilter(value)}
-        value={typeFilter}
-        placeholder="Filter"
-        allowClear
-      >
-        <Option value="">All</Option>
-        <Option value="income">Income </Option>
-        <Option value="expense">Expense </Option>
-      </Select>
-      <Radio.Group
-        className="input-radio"
-        onChange={(e) => setSortKey(e.target.value)}
-        value={sortKey}
-      >
-        <Radio.Button value="">No sort</Radio.Button>
-        <Radio.Button value="date">Sort by Date</Radio.Button>
-        <Radio.Button value="amount">Sort by Amount</Radio.Button>
-      </Radio.Group>
-      <Table dataSource={sortedTransactions} columns={columns} />;
-    </>
+      <div>
+        <Radio.Group
+          className="input-radio"
+          onChange={(e) => setSortKey(e.target.value)}
+          value={sortKey}
+        >
+          <Radio.Button value="">No sort</Radio.Button>
+          <Radio.Button value="date">Sort by Date</Radio.Button>
+          <Radio.Button value="amount">Sort by Amount</Radio.Button>
+        </Radio.Group>
+        <Table dataSource={sortedTransactions} columns={columns} />;
+      </div>
+    </div>
   );
 }
 
