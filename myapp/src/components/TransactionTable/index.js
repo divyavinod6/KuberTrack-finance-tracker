ea4760e4d3d5213933fbc6770e7f9818fd0f94a1
divyavinod6 <thisisdivyavinod@gmail.com>
2024-10-06 23:55:23 +0530

    updated export csv

--- myapp/src/components/TransactionTable/index.js
+++ myapp/src/components/TransactionTable/index.js
@@ -47,12 +47,14 @@ function TransactionTable({ transactions }) {
       fields: ['Name', 'type', 'tag', 'date', 'amount'],
       transactions,
     });
-    var data = new Blob([csv], { type: 'text/csv;charset=utf-8;' });
-    var csvURL = window.URL.createObjectURL(data);
-    tempLink = document.createElement('a');
-    tempLink.href = csvURL;
-    tempLink.setAttribute('download', 'transactions.csv');
-    tempLink.click();
+    const blob = new Blob([csv], { type: 'text/csv;charset=utf-8;' });
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = 'transactions.csv';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
   }
 
   let sortedTransactions = filteredTransactions.sort((a, b) => {
